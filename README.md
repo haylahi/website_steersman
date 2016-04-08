695c1b8670d2ccb038925bda796cd9e3e766b68a
Art Shipulin <art.shipulin@steersman.works>
2016-04-08 17:22:32 -0500

    Let there be Steersman Website Fixes and Optimization module for Odoo 9 Enterprise

---
+++ README.md
@@ -0,0 +1,3 @@
+# Steersman Website Fixes and Optimization
+
+This Odoo 9 Enterprise module is used to apply fixes and optimization to all instances managed by Steersman that use `website_enterprise` module.
---
+++ __openerp__.py
@@ -0,0 +1,11 @@
+{
+	'name': 'Steersman Website Fixes and Optimization',
+	'category': 'Website',
+	'version': '0.0.1',
+	'author': 'Steersman Company',
+	'website': '//steersman.works',
+	'depends': ['website_enterprise'],
+	'data': ['data/website_enterprise.assets_frontend.xml'],
+	'application': False,
+	'installable': True
+}
